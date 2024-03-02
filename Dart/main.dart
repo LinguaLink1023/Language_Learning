fb48f844057e5cc90dcedfea53cdee8f6c97a34c
LigualLink <lixiangban1023@qq.com>
2024-03-02 19:52:22 +0800

    dart学习

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,4 +1,13 @@
+import "dart:io";
 
 void main() {
   print("Hello dart!\n");
+  var name = "Jerry";
+  var age = 39;
+  var height = 100000000.81456 + 0.1;
+  var height2 = height - 100000000.81456;
+  print("Hello, I'm $name. I'm $age years old\n, my height is ${9 ~/ 7} cm");
+  print("Please enter your wife's height: ");
+  var myWifeHeight = stdin.readLineSync();
+  print("My wife's height: ${myWifeHeight}");
 }
