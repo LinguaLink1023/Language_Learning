2dda7f69da416b23b09776154f4cb7a5b7387e58
LinguaLink1023 <lixiangban1023@qq.com>
2024-03-05 18:57:28 +0800

    Dart 基础表达式

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,13 +1,10 @@
 import "dart:io";
 
 void main() {
-  print("Hello dart!\n");
-  var name = "Jerry";
-  var age = 39;
-  var height = 100000000.81456 + 0.1;
-  var height2 = height - 100000000.81456;
-  print("Hello, I'm $name. I'm $age years old\n, my height is ${9 ~/ 7} cm");
-  print("Please enter your wife's height: ");
-  var myWifeHeight = stdin.readLineSync();
-  print("My wife's height: ${myWifeHeight}");
+  var my_list = const [1, 2, 3];
+  // my_list[1] = 1;
+  // print(my_list);
+  my_list = [1, 2];
+  print(my_list);
+
 }
