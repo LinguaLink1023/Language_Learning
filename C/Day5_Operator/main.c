4789d3ad91b56f42e829fa3b5fadf7e1da6cab25
LinguaLink1023 <lixiangban1023@qq.com>
2024-03-10 21:08:25 +0800

    Windows上的第一次提交，C语言的switch-case

--- C/Day5_Operator/main.c
+++ C/Day5_Operator/main.c
@@ -6,13 +6,19 @@
 typedef double real;
 
 int main() {
-    bool canDo = true;
-    int number = 0;
-    if (canDo) {
-        number = 100;
-    } else {
-        number = 59;
+    char ch = 'a';
+    switch (ch) {
+        case 'a':
+            printf("Apple\n");
+            break;
+        case 'b':
+            printf("Banana\n");
+            break;
+        case 'c':
+            printf("Cherry\n");
+            break;
+        default:
+            printf("Last one\n");
+            break;
     }
-    printf("number = %d\n", number);
-    return 0;
 }
